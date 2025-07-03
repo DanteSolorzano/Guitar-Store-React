55668eea68e522e8fa9c74f75d2405654e002a17
DanteSolorzano <solorzanodante19@gmail.com>
2025-07-02 21:48:55 -0600

    I translate to english the content of a button

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -15,7 +15,7 @@ export default function Guitar( {guitar, addToCart} ) {
                         type="button"
                         className="btn btn-dark w-100"
                         onClick={() => addToCart(guitar)}
-                    >Agregar al Carrito</button>
+                    >Add to Cart</button>
                 </div>
             </div>
     )
